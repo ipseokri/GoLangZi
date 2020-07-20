17f45851256ee3e19820965dde632c8f86bdf1c3
ipseokri <sound600@naver.com>
2020-07-20 13:57:01 +0900

    rev2 update 2020.07.20 1Commit

--- ch4/rev/rev2.go
+++ ch4/rev/rev2.go
@@ -1,7 +1,7 @@
 package rev
 
-func reverse2(s *int){
-	for i, j := 0, len(s)-1; i < j; i , j = i+1, j-1{
-		s[i], s[j] = s[j], s[i]
+func reverse2(s *[]int){
+	for i, j := 0, len(*s)-1; i < j; i , j = i+1, j-1{
+		(*s)[i],(*s)[j] = (*s)[j], (*s)[i]
 	}
 }
