8e870f0c96db9e39560ad4ad16f0aee9ac927cad
ipseokri <sound600@naver.com>
2020-07-21 17:05:07 +0900

    2020.07.21 1Commit

---
+++ ch4/movie/moive.go
@@ -0,0 +1,55 @@
+package main
+
+
+// cmd 상에서 go run 했을때는 실행이 되고 IDE를 통한 go run 은 안되는 이유가 뭘까?-
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Movie struct{
+	Title string
+	Year int `json:"released"`
+	Color bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphrey Bogart", "InGrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true, Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true, Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	// ...
+}
+
+
+func main() {
+	{
+		//!+Marshal
+		data, err := json.Marshal(movies)
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		fmt.Printf("%s\n", data)
+		//!-Marshal
+	}
+
+	{
+		//!+MarshalIndent
+		data, err := json.MarshalIndent(movies, "", "    ")
+		if err != nil {
+			log.Fatalf("JSON marshaling failed: %s", err)
+		}
+		fmt.Printf("%s\n", data)
+		//!-MarshalIndent
+
+		//!+Unmarshal
+		var titles []struct{ Title string }
+		if err := json.Unmarshal(data, &titles); err != nil {
+			log.Fatalf("JSON unmarshaling failed: %s", err)
+		}
+		fmt.Println(titles) // "[{Casablanca} {Cool Hand Luke} {Bullitt}]"
+		//!-Unmarshal
+	}
+}
