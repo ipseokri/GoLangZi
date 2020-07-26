8c4dd96ee9e3fe42fa9a4fb0fec9101ca9eb6204
ipseokri <sound600@naver.com>
2020-07-26 21:20:02 +0900

    2020.07.26 - squares - ~ing

---
+++ ch5/squares/squares.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//squares 는 호출될 때마다 다음번 제곱값을 반환하는 함수를 반환한다.
+func squares() func() int{
+	var x int
+	return func () int{
+		x ++
+		return x * x
+	}
+}
+
+func main(){
+	f := squares()
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+	fmt.Println(f())
+}
